c3e4f906b7c9a09ad88c876e7f2017d879388cf0
Alyssa Burke <alyssa.burke@personifyinc.com>
2015-11-01 18:53:37 -0600

    initial commit after transfering to the new laptop

---
+++ app/src/main/java/com/fingerspellingtutor/fingerspellingtutor/GameActivity.java
@@ -0,0 +1,67 @@
+
+
+package com.fingerspellingtutor.fingerspellingtutor;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//import butterknife.Bind;
+//import butterknife.ButterKnife;
+
+
+
+/**
+ * Created by alyssaburke on 10/8/15.
+ */
+
+public class GameActivity extends FragmentActivity {
+
+    CallbackManager callbackManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+
+        Button levelonecompleteButton = (Button) findViewById(R.id.levelonecomplete_button);
+        setContentView(R.layout.activity_levelone);
+
+    }
+
+}
---
+++ app/src/main/java/com/fingerspellingtutor/fingerspellingtutor/MainActivity.java
@@ -0,0 +1,115 @@
+
+package com.fingerspellingtutor.fingerspellingtutor;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//import butterknife.Bind;
+//import butterknife.ButterKnife;
+
+/**
+ * Created by alyssaburke on 10/8/15.
+ */
+public class MainActivity extends FragmentActivity {
+
+    CallbackManager callbackManager;
+
+   // Button startgameButton = (Button) findViewById(R.id.startgame_button);
+
+   // @Bind(R.id.login_button) LoginButton loginButton;
+
+    /*
+    @Bind(view id)
+    Class nameOfObject
+    */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+
+
+
+        setContentView(R.layout.activity_login_fb);
+       // ButterKnife.bind(this);
+
+        // Old shitty way of referencing view
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        callbackManager = CallbackManager.Factory.create();
+        //loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                // App code
+
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        setContentView(R.layout.activity_dashboard);
+       // ButterKnife.bind(this);
+        Button startgameButton = (Button) findViewById(R.id.startgame_button);
+
+    }
+
+    public void startGame (View view)
+    {
+        Intent intent = new Intent(MainActivity.this, GameActivity.class);
+        startActivity(intent);
+    }
+}
